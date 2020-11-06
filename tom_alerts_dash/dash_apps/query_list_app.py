630649db3e6bf37eba64f921394a59e1d5efa60d
David Collom <dcollom@lco.global>
2020-11-06 15:08:13 -0800

    Added header and modified filtering

--- tom_alerts_dash/dash_apps/query_list_app.py
+++ tom_alerts_dash/dash_apps/query_list_app.py
@@ -15,38 +15,19 @@ from tom_alerts.alerts import get_service_class, get_service_classes
 app = DjangoDash('BrokerQueryListViewDash', external_stylesheets=[dbc.themes.BOOTSTRAP], add_bootstrap_links=True)
 
 
-# operators = [['ge ', '>='], ['le ', '<='], ['lt ', '<'], ['gt ', '>'], ['ne ', '!='], ['eq ', '='], ['contains ']]
-
-
-def split_filter_part(filter_part):
-    for operator_type in operators:
-        for operator in operator_type:
-            if operator in filter_part:
-                name_part, value_part = filter_part.split(operator, 1)
-                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
-
-                value_part = value_part.strip()
-                v0 = value_part[0]
-                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
-                    value = value_part[1: -1].replace('\\' + v0, v0)
-                else:
-                    try:
-                        value = float(value_part)
-                    except ValueError:
-                        value = value_part
-
-                # word operators need spaces after them in the filter string,
-                # but we don't want these later
-                return name, operator_type[0].strip(), value
-
-    return [None] * 3
-
-
 class BrokerClient:
     _broker = None
 
     def __init__(self, broker_name, *args, **kwargs):
         self._broker = get_service_class(broker_name)()
+
+    @property
+    def broker(self):
+        return self._broker
+
+    @broker.setter
+    def broker(self, new_broker_name):
+        self._broker = get_service_class(new_broker_name)()
 
     def get_columns(self):
         return self._broker.get_dash_columns()
@@ -54,17 +35,15 @@ class BrokerClient:
     def get_alerts(self, parameters):
         return self._broker.get_dash_data(parameters)
 
-    def set_broker(self, new_broker_name):
-        self._broker = get_service_class(new_broker_name)()
 
-
-broker_client = BrokerClient('MARS')
-print(get_service_classes())
-print([{'label': clazz, 'value': clazz} for clazz in get_service_classes().keys()])
+broker_client = BrokerClient('SCIMMA')
 
 
 app.layout = dbc.Container([
     dhc.Div([
+        dhc.Div(
+            dhc.H3('Browse Alerts')
+        ),
         dhc.Div(
             dhc.P(
                 dcc.Dropdown(
@@ -115,14 +94,14 @@ def alerts_table_filter(broker_selection, filter_query):
     print(filter_query)
     if broker_selection:
         print(broker_selection)
-        broker_client.set_broker(broker_selection)
+        broker_client.broker = broker_selection
 
     parameters = {}
     if filter_query:
         for filter_part in filter_query.split(' && '):
             col_name, operator, filter_value = filter_part.split()
             col_name = col_name[col_name.find('{') + 1: col_name.rfind('}')]
-            parameters[col_name] = (operator, filter_value)
+            parameters[col_name] = {'operator': operator, 'value': filter_value}
     print(f'parameters: {parameters}')
 
     return broker_client.get_columns(), broker_client.get_alerts(parameters)
