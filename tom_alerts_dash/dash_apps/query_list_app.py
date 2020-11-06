8a5d647c2d3c0e57ce82d9b577cb074ab59fa267
David Collom <dcollom@lco.global>
2020-11-06 13:01:02 -0800

    Added filtering support

--- tom_alerts_dash/dash_apps/query_list_app.py
+++ tom_alerts_dash/dash_apps/query_list_app.py
@@ -1,3 +1,5 @@
+import re
+
 import dash
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
@@ -13,7 +15,7 @@ from tom_alerts.alerts import get_service_class, get_service_classes
 app = DjangoDash('BrokerQueryListViewDash', external_stylesheets=[dbc.themes.BOOTSTRAP], add_bootstrap_links=True)
 
 
-operators = [['ge ', '>='], ['le ', '<='], ['lt ', '<'], ['gt ', '>'], ['ne ', '!='], ['eq ', '=']]
+# operators = [['ge ', '>='], ['le ', '<='], ['lt ', '<'], ['gt ', '>'], ['ne ', '!='], ['eq ', '='], ['contains ']]
 
 
 def split_filter_part(filter_part):
@@ -49,8 +51,8 @@ class BrokerClient:
     def get_columns(self):
         return self._broker.get_dash_columns()
 
-    def get_alerts(self):
-        return self._broker.get_dash_data({})
+    def get_alerts(self, parameters):
+        return self._broker.get_dash_data(parameters)
 
     def set_broker(self, new_broker_name):
         self._broker = get_service_class(new_broker_name)()
@@ -74,23 +76,23 @@ app.layout = dbc.Container([
         ),
         dhc.Div(
             dhc.P(
-                dbc.Button('Create targets from selected')
+                dbc.Button('Create targets from selected', className='btn btn-outline-primary')
             )
         ),
         dhc.Div(  # Filters go here
-            dcc.Input(
-                id='name-search',
-                type='text',
-                placeholder='Alert name search',
-                debounce=True
-            ),
+            # dcc.Input(
+            #     id='name-search',
+            #     type='text',
+            #     placeholder='Alert name search',
+            #     debounce=True
+            # ),
             id='alerts-table-filters-container'
         ),
         dhc.Div(  # Alerts datatable goes here
             DataTable(
                 id='alerts-table',
                 columns=broker_client.get_columns(),
-                data=broker_client.get_alerts(),
+                data=broker_client.get_alerts({}),
                 filter_action='custom',
                 row_selectable='multi',
                 page_current=0,
@@ -107,17 +109,20 @@ app.layout = dbc.Container([
     [Output('alerts-table', 'columns'),
      Output('alerts-table', 'data')],
     [Input('broker-selection', 'value'),
-     Input('name-search', 'value'),
      Input('alerts-table', 'filter_query')]
 )
-def alerts_table_filter(broker_selection, name_search, filter_query):
+def alerts_table_filter(broker_selection, filter_query):
     print(filter_query)
     if broker_selection:
         print(broker_selection)
         broker_client.set_broker(broker_selection)
 
-    for filter_part in filter_query.split(' && '):
-        if operator in operators:
-            pass
+    parameters = {}
+    if filter_query:
+        for filter_part in filter_query.split(' && '):
+            col_name, operator, filter_value = filter_part.split()
+            col_name = col_name[col_name.find('{') + 1: col_name.rfind('}')]
+            parameters[col_name] = (operator, filter_value)
+    print(f'parameters: {parameters}')
 
-    return broker_client.get_columns(), broker_client.get_alerts()
+    return broker_client.get_columns(), broker_client.get_alerts(parameters)
