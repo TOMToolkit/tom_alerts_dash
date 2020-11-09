b8243ff1cb80516a4df1aa481969548dbf83c363
David Collom <dcollom@lco.global>
2020-11-09 12:45:56 -0800

    Overriding base tomtoolkit BrokerQueryListView to include a link to browse alerts

--- tom_alerts_dash/urls.py
+++ tom_alerts_dash/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from tom_alerts_dash import dash
-from tom_alerts_dash.views import BrokerQueryBrowseView, RunQueryView
+from tom_alerts_dash.views import BrokerQueryBrowseView, BrokerQueryListView, RunQueryView
 
 app_name = 'tom_alerts_dash'
 
 urlpatterns = [
+    path('query/list/', BrokerQueryListView.as_view(), name='list'),
     path('query/browse/', BrokerQueryBrowseView.as_view(), name='browse'),
     path('query/<int:pk>/run', RunQueryView.as_view(), name='run')
 ]
--- tom_alerts_dash/views.py
+++ tom_alerts_dash/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from tom_alerts.views import BrokerQueryListView
+
 
 # class BrokerQueryListView(LoginRequiredMixin, TemplateView):
 class BrokerQueryBrowseView(TemplateView):
@@ -9,3 +11,7 @@ class BrokerQueryBrowseView(TemplateView):
 
 class RunQueryView(TemplateView):
     template_name = 'tom_alerts_dash/brokerquery_browse.html'
+
+
+class BrokerQueryListView(BrokerQueryListView):
+    template_name = 'tom_alerts_dash/brokerquery_list.html'
